58c7a09d6135364bb720f7e232076f29fb7136ae
moss0 <2317919@students.ucreative.ac.uk>
2026-03-02 17:23:08 +0000

    Trying to do JSON using c++ instead

---
+++ Source/Greedy_Piggies/Private/QA_Json.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QA_Json.h"
+#include "Misc/FileHelper.h"
+
+QA_Json::QA_Json()
+{
+}
+
+QA_Json::~QA_Json()
+{
+}
+
+// 1. Define your data structure
+USTRUCT(BlueprintType)
+struct FMyGameData
+{
+    GENERATED_BODY()
+
+    UPROPERTY()
+    FString PlayerName;
+
+    UPROPERTY()
+    int32 Score;
+};
+
+// 2. Serialize and Save at Runtime
+void SaveJson()
+{
+    FMyGameData Data;
+    Data.PlayerName = "Hero";
+    Data.Score = 1500;
+
+    FString JsonString;
+    // Industry standard: Convert Struct -> JSON String
+    if (FJsonObjectConverter::UStructToJsonObjectString(Data, JsonString))
+    {
+        FString SavePath = FPaths::ProjectSavedDir() + TEXT("SaveData.json");
+        FFileHelper::SaveStringToFile(JsonString, *SavePath);
+    }
+}
